7f05411d66555f333d3696acb4b327bbb4f46365
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-01-27 11:43:20 +0530

    added Qr Code

---
+++ README.md
@@ -0,0 +1,6 @@
+Qr Code Generator
+Genrates Qr code from passed text or link, and save them as PNG image format
+
+Requires:
+- pyqrcode Library (pip install pyqrcode)
+- ttkthemes Library (pip install ttkthemes)
---
+++ qr.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import filedialog 
+from tkinter import ttk
+import ttkthemes            #3rd party library, install : pip install ttkthemes
+import pyqrcode                         #3rd party library, install : pip install pyrcode
+import os
+
+class Qr_code_generator:
+
+    def create_qr(self,text):
+        #creating Qr code 
+        qr = pyqrcode.create(text)
+
+        #asking user for file name and directory
+        file = filedialog.asksaveasfile(mode = 'wb',defaultextension = ".png",filetypes = (("Portable Network Graphics","*.png"), ("All Files","*.*")))
+        if file:
+            qr.png(file, scale=8)
+        return
+    #method to create GUI window    
+    def create_window(self,width,height):
+        root = ttkthemes.ThemedTk(theme = "plastik") #creating window and setting theme for it
+        root.title("Qr Code Generator")             #window title
+        root.geometry(f"{width}x{height}")      #window dimension
+        
+        fr = tk.Frame(root)                 #frame inside GUI window
+        
+        fr.pack()
+        #variable to store Entry Box Content
+        text = tk.StringVar()
+        input = ttk.Entry(fr,textvariable = text,width = 30)      #Entry Box
+        input.grid(row = 1,column = 0,pady = 30,ipady = 6)  
+        
+        #Generate Button
+        create_button = ttk.Button(fr,text = "Create",command = lambda :  self.create_qr(text.get()))
+        create_button.grid(row = 2,column = 0,ipady = 5,ipadx = 5)
+        root.mainloop()
+        
+
+qr_code = Qr_code_generator()           #object of Qr_code_generator class
+qr_code.create_window(400,400)          #method calling
